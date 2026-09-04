435c210e1be743434c8247d583da0b7e14cc6390
Lucas Tian <z5768673@ad.unsw.edu.au>
2026-09-04 16:28:00 +1000

    feat: implement switch button

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const switchButton = document.getElementById('switch-button');
 const daySection = document.getElementById('day-section');
 const nightSection = document.getElementById('night-section');
+// Day section 
+const pomodoro = document.getElementById('Pomodoro');
+const startButton = document.getElementById('start-button');
+const pauseButton = document.getElementById('pause-button');
+const resetButton = document.getElementById('reset-button');
+// Night section 
+const lofiButton = document.getElementById('lofi-sounds');
+const lofiAudio = document.getElementById('lofi-audio');
+
+switchButton.addEventListener('click', () => {
+    document.body.classList.toggle('night-mode');
+});
