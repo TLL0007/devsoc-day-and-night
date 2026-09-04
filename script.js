3271605f8ac8662503471b438245d467445c2119
Lucas Tian <z5768673@ad.unsw.edu.au>
2026-09-04 16:02:03 +1000

    feat: added DOM elements in JS and setup an initial style.css

---
+++ script.js
@@ -0,0 +1,3 @@
+const switchButton = document.getElementById('switch-button');
+const daySection = document.getElementById('day-section');
+const nightSection = document.getElementById('night-section');
